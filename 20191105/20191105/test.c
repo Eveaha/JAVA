b5b06d1f13532602f541fca7b1953c4d824207c3
Eve <935236069@qq.com>
2019-11-10 17:27:32 +0800

    。

---
+++ 20191105/20191105/test.c
@@ -0,0 +1,72 @@
+
+
+//int main()
+//{
+//	printf("dfsfafa");
+//}
+
+#include<stdio.h>
+int main()
+{
+	/*	int i = 0;
+	int j = 0;
+	for (i = 0;i < 10 ;i++)
+	{
+	for (j = 0; j < 10; j++){
+	printf("hehe\n");
+	}
+	}
+	return 0;
+	system(pause);*/
+
+	//int i = 1;
+	//do{
+	//	printf("%d", i);
+	//	i++;
+	//}
+	//while (i <= 10);
+	//system(pause);
+	//return 0;
+	//}
+
+
+	//求n的阶乘——求1-10的阶乘相加
+	//int n = 0;
+	//int i = 0;
+	//int ret = 1;
+	//int sum = 0;
+	/*scanf("%d", &n);*/
+	/*for (n = 1; n <= 10; n++)
+	{
+		ret = 1;
+		for (i = 1; i <= n; i++)
+		{
+			ret *= i;
+		}
+		sum += ret;
+	}
+
+	printf("%d\n", ret);
+
+
+	return 0;*/
+/*
+	for (n = 1; n <= 10; n++)
+	{
+		
+		ret *= n;
+		sum += ret;
+	}
+
+	printf("%d\n", ret);
+	return 0;*/
+
+
+
+
+
+
+
+
+
+}
